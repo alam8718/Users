e282b9cc3747ce614dd1952d44ebaf5222627953
Mahabub Alam <mahabubalam3004@gmail.com>
2024-02-12 16:33:58 +0600

    ...

--- src/Components/users/Users.jsx
+++ src/Components/users/Users.jsx
@@ -9,13 +9,13 @@ function Users() {
   return (
     <>
       <NavBar />
-      <div className="py-10 w-full h-[calc(100vh-115px)] overflow-y-scroll max-mobile:flex max-mobile:flex-wrap max-mobile: gap-5 max-mobile:gap-0 max-mobile:justify-center   ">
+      <div className="py-10 w-full h-[calc(100vh-115px)] overflow-y-scroll flex flex-wrap  gap-0  justify-center ">
         {users.map((user, index) => (
           <Link
             to={`/user/${user?.id}`}
             target="_blank"
             key={index}
-            className=" my-3 w-[365px] sm:w-auto flex justify-center flex-wrap ">
+            className=" my-3 w-full max-w-[365px] sm:w-auto flex justify-center flex-wrap ">
             <SingleUserCard user={user} />
           </Link>
         ))}
